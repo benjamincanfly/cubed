14807db2e528f03f0e2236c1078d1c9e14bf7fde
Benjamin <benjamincanfly@gmail.com>
2014-06-17 12:32:30 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CUBED Web Series</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+		<div id="content">
+		        <iframe width="1280" height="720" src="//www.youtube.com/embed/3Se1AETh224?list=PLlp-gxEi1t6Gof5wkceTsR8D2zNEGEhCG" frameborder="0" allowfullscreen></iframe>
+				<div id="text">
+					<p><b>CUBED</b> airs every Wednesday starting June 25th, 2014 at <a href="http://www.cubedseries.com">CubedSeries.com</a> and at <a href="http://www.youtube.com/benjaminapple">YouTube.com/BenjaminApple</a>.</p>
+					<p>Written and directed by Benjamin Apple. Featuring: <a href="http://twitter.com/benjaminapple">Benjamin Apple</a>, <a href="http://twitter.com/dblackattack">Dan Black</a>, <a href="http://twitter.com/robmichaelhugel">Rob Michael Hugel,</a> <a href="http://twitter.com/DianimalK">Diana Kolsky</a>, <a href="http://twitter.com/timmartinwhy">Tim Martin</a>, <a href="http://twitter.com/vaov">Veronica Osorio</a>, and <a href="http://twitter.com/gavinspeiller">Gavin Speiller</a>. Director of Photography: <a href="http://adamsacks.com/">Adam Sacks</a>. Sound Mixer: <a href="https://pro-labs.imdb.com/name/nm3956849">Thomas Zaccheo</a>. Production Assistant: <a href="http://www.beachethan.com/">Ethan Beach</a>.</p>
+					<p>Contact: <a href="mailto:ben@benjaminapple.com">ben@benjaminapple.com</a></p>
+				</div>
+		</div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-1029946-16', 'cubedseries.com');
+	  ga('send', 'pageview');
+	</script>
+    </body>
+</html>
